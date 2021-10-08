3ac4991cab4213ba09d2e2a48b92a6edb6d424a9
Ricky Mormor <rickymormor1@gmail.com>
2021-10-08 09:50:42 +0000

    Generate project section dynamically

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const navLink = document.querySelectorAll('.nav-link');
+const projectSection = document.querySelector('.container');
 
 //Menu button
 hamburger.addEventListener('click', () => {
@@ -13,3 +14,11 @@ navLink.forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+
+const projectInfo = {
+  title: 'Project name goes <span>here</span>',
+  tech1: 'HTML/CSS',
+  tech2: 'css',
+  tech3: 'Ruby on Rails',
+};
